4756d16be2087b69b37c0143f1487e29319d307e
The-Sch-O <118555410+The-Sch-O@users.noreply.github.com>
2024-03-06 09:23:10 +0100

    Refactoring pour code plus propre

--- LectureDonnees.py
+++ LectureDonnees.py
@@ -4,6 +4,8 @@ import pickle
 import time
 import os
 
+from FretSncf_grp4.Util import InstanceSheetNames
+
 # Chemin d'accès du fichier contenant l'instance
 instance = "mini_instance"
 instance_file = instance + ".xlsx"
@@ -14,28 +16,13 @@ def load_instance(file_path) -> dict:
     Charge l'instance du problème de la gare de fret donnée par `file_path` et crée un dictionnaire qui stocke toutes le données pertinentes
     """
     all_dict = dict()
-
-    chantiers = pd.read_excel(file_path, sheet_name="Chantiers")
-    all_dict["Chantiers"] = chantiers
-
-    machines = pd.read_excel(file_path, sheet_name="Machines")
-    all_dict["Machines"] = machines
-
-    arrivees = pd.read_excel(file_path, sheet_name="Sillons arrivee")
-    all_dict["Sillons arrivee"] = arrivees
-
-    departs = pd.read_excel(file_path, sheet_name="Sillons depart")
-    all_dict["Sillons depart"] = departs
-
-    correspondances = pd.read_excel(file_path, sheet_name="Correspondances")
-    all_dict["Correspondances"] = correspondances
-
-    taches_humaines = pd.read_excel(file_path, sheet_name="Taches humaines")
-    all_dict["Taches humaines"] = taches_humaines
-
-    roulements_agents = pd.read_excel(file_path, sheet_name="Roulements agents")
-    all_dict["Roulements agents"] = roulements_agents
-
+    all_dict[InstanceSheetNames.SHEET_CHANTIERS] = pd.read_excel(file_path, sheet_name=InstanceSheetNames.SHEET_CHANTIERS)
+    all_dict[InstanceSheetNames.SHEET_MACHINES] = pd.read_excel(file_path, sheet_name=InstanceSheetNames.SHEET_MACHINES)
+    all_dict[InstanceSheetNames.SHEET_ARRIVEES] = pd.read_excel(file_path, sheet_name=InstanceSheetNames.SHEET_ARRIVEES)
+    all_dict[InstanceSheetNames.SHEET_DEPARTS] = pd.read_excel(file_path, sheet_name=InstanceSheetNames.SHEET_DEPARTS)
+    all_dict[InstanceSheetNames.SHEET_CORRESPONDANCES] = pd.read_excel(file_path, sheet_name=InstanceSheetNames.SHEET_CORRESPONDANCES)
+    all_dict[InstanceSheetNames.SHEET_TACHES] = pd.read_excel(file_path, sheet_name=InstanceSheetNames.SHEET_TACHES)
+    all_dict[InstanceSheetNames.SHEET_ROULEMENTS] = pd.read_excel(file_path, sheet_name=InstanceSheetNames.SHEET_ROULEMENTS)
     return all_dict
 
 def save_to_pickle(data, pkl_file_path):
@@ -63,14 +50,6 @@ if not os.path.isfile(instance_pickle_file):
     save_to_pickle(data_dict, instance_pickle_file)
 else:
     data_dict = load_from_pickle(instance_pickle_file)
-
-
-def make_train_id_unique():
-    arrivees = data_dict["Sillons arrivee"]
-    couple_unique = ["JARR", "n°TRAIN"]
-    
-    pass
-
 
 if __name__ == "__main__":
     print(data_dict.keys())
---
+++ Util.py
@@ -0,0 +1,21 @@
+
+ORDERED_MACHINES = ["DEB", "FOR", "DEG"]
+
+class InstanceSheetNames:
+    SHEET_CHANTIERS = "Chantiers"
+    SHEET_MACHINES = "Machines"
+    SHEET_ARRIVEES = "Sillons arrivee"
+    SHEET_DEPARTS = "Sillons depart"
+    SHEET_CORRESPONDANCES = "Correspondances"
+    SHEET_TACHES = "Taches Humaines"
+    SHEET_ROULEMENTS = "Roulements agents"
+
+class ArrivalColumnNames:
+    ARR_TRAIN_NUMBER = "n°TRAIN"
+    ARR_DATE = "JARR"
+    ARR_HOUR = "HARR"
+
+class DepartureColumnNames:
+    DEP_TRAIN_NUMBER = "n°TRAIN"
+    DEP_DATE = "JDEP"
+    DEP_HOUR = "HDEP"
