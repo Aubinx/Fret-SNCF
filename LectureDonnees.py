134bb4e8d30932481085aedd4f401f8054b726c8
The-Sch-O <118555410+The-Sch-O@users.noreply.github.com>
2024-03-08 13:45:48 +0100

    Update LectureDonnees.py
    ajout de commentaires sur l'utilité de chaque fonction

--- LectureDonnees.py
+++ LectureDonnees.py
@@ -29,6 +29,11 @@ def load_instance(file_path) -> dict:
     return all_dict
 
 def get_first_day(data):
+    """
+    Recherche dans l'instance `data` et renvoie le premier jour.
+    Particulièrement utile pour transformer les couples (jour, heure) d'arrivée ou de départ d'un train
+    en un créneau stocké sur un entier tel qu'utilisé dans le module `Horaires.py`.
+    """
     first_day = datetime.date.max
     for _, row in data[InstanceSheetNames.SHEET_ARRIVEES].iterrows():
         jour = datetime.datetime.strptime(row[ArriveesColumnNames.ARR_DATE], '%d/%m/%Y').date()
@@ -37,6 +42,11 @@ def get_first_day(data):
     return first_day
 
 def dates_to_creneaux(data):
+    """
+    Parcourt les données de l'instance et ajoute une colonne "Creneau" 
+    dans le dataframe contenant les trains à l'arrivée et au départ 
+    qui stocke leur horaire d'arrivée sous la forme d'un créneau entier
+    """
     first_day = get_first_day(data)
     # Create new column with default value of 0
     data[InstanceSheetNames.SHEET_ARRIVEES]["Creneau"] = 0
@@ -77,15 +87,14 @@ def save_to_pickle(data, pkl_file_path):
 
 def load_from_pickle(pkl_file_path):
     """
-    charge (si possible) le fichier donné par `pkl_file_path` et le renvoie
+    Si possible, charge le fichier donné par `pkl_file_path` et le renvoie
+    Sinon, renvoie None.
     """
     if os.path.isfile(pkl_file_path):
         return pickle.load(open(pkl_file_path, 'rb'))
     else:
         print("Ce fichier n'existe pas")
         return None
-
-
 
 data_dict = None
 if not os.path.isfile(instance_pickle_file):
@@ -101,7 +110,7 @@ else:
 def composition_train_depart(data, id_train_depart):
     """
     argument : `id_train_depart` est l'identifiant unique du train de départ considéré
-    returns : `related_trains` la liste des trains à l'arrivées qui contiennent un wagon qui est nécessaire pour le train au départ considéré
+    returns : `related_trains` la liste des trains à l'arrivées qui contiennent un wagon faisant partie du train au départ considéré
     """
     related_trains = []
     correspondances = data[InstanceSheetNames.SHEET_CORRESPONDANCES]
@@ -114,11 +123,13 @@ def composition_train_depart(data, id_train_depart):
     return related_trains
 
 
-
-
 def indispo_machine_to_intervalle(data, machine):
+    """
+    Parcourt dans l'instances les indisponibiltés machines de la machine `machine`
+    Renvoie cette indisponibilité sous la forme d'une liste de couple (`debut`, `fin`) qui indiquent les créneaux de début et de fin de chaque indisponibilité
+    """
     machine_data = data[InstanceSheetNames.SHEET_MACHINES]
-    for index, row in machine_data.iterrows():
+    for _, row in machine_data.iterrows():
         if row[MachinesColumnNames.MACHINE_NAME] == machine:
             total_indisp = row[MachinesColumnNames.MACHINE_INDISPONIBILITES]
             list_indisp = total_indisp.split(sep=";")
@@ -134,7 +145,10 @@ def indispo_machine_to_intervalle(data, machine):
     return None
 
 def indispo_chantier_to_intervalle(data, chantier):
-    
+    """
+    Parcourt dans l'instances les indisponibiltés chantiers du chaniter `chantier`
+    Renvoie cette indisponibilité sous la forme d'une liste de couple (`debut`, `fin`) qui indiquent les créneaux de début et de fin de chaque indisponibilité
+    """
     chantier_data = data[InstanceSheetNames.SHEET_CHANTIERS]
     for index, row in chantier_data.iterrows():
         if row[ChantiersColumnNames.CHANTIER_NAME] == chantier:
