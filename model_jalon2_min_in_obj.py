ca5cbf28f83315e88d068bd32cd624363328601e
Richard Maxence <maxence.richard@student-cs.fr>
2024-03-15 16:56:14 +0100

    Ajout du minimum en fonction

--- model_jalon2_min_in_obj.py
+++ model_jalon2_min_in_obj.py
@@ -1,31 +1,33 @@
 
 from gurobipy import *
 
-from model import MODEL, DEPARTS, DepartsColumnNames, VARIABLES, DATA_DICT, CONTRAINTES
+from model import DEPARTS, DepartsColumnNames, DATA_DICT
 from lecture_donnees import composition_train_depart
 
-# Variables de décision concernant les trains au départ :
-EPSILON_MINIMUM = LinExpr()
-for index in DEPARTS.index:
-    jour = DEPARTS[DepartsColumnNames.DEP_DATE][index]
-    numero = DEPARTS[DepartsColumnNames.DEP_TRAIN_NUMBER][index]
+
+def model_jalon2_min_in_obj(model, variables, contraintes):
+    """
+    Implementation du minimum pour le premier wagon arriant au chantier FOR
     
-    VARIABLES[f"min_DEB_{jour}_{numero}"] = MODEL.addVar(
-        name = f"min_DEB_{jour}_{numero}",
-        vtype = GRB.INTEGER,
-        lb = 0
-    )
-    EPSILON_MINIMUM += VARIABLES[f"min_DEB_{jour}_{numero}"]
-    for related_train in composition_train_depart(DATA_DICT, (jour, numero)):
-        date, id_name = related_train
-        CONTRAINTES[f"min_DEB_depart_{jour}_{numero}_arrivee_{date}_{id_name}_MINIMUM"] = MODEL.addConstr(
-        VARIABLES[f"min_DEB_{jour}_{numero}"] <= VARIABLES[f"Train_ARR_{date}_{id_name}_DEB"],
-        name = f"min_DEB_depart_{jour}_{numero}_arrivee_{date}_{id_name}_MINIMUM"
-    )
+    Retourne l'expression Gurobi à MAXIMISER 
+    """
+    # Variables de décision concernant les trains au départ :
+    epsilon_minimum = LinExpr()
+    for index in DEPARTS.index:
+        jour = DEPARTS[DepartsColumnNames.DEP_DATE][index]
+        numero = DEPARTS[DepartsColumnNames.DEP_TRAIN_NUMBER][index]
 
-MODEL.setObjective(EPSILON_MINIMUM, GRB.MAXIMIZE)
+        variables[f"min_DEB_{jour}_{numero}"] = model.addVar(
+            name = f"min_DEB_{jour}_{numero}",
+            vtype = GRB.INTEGER,
+            lb = 0
+        )
+        epsilon_minimum += variables[f"min_DEB_{jour}_{numero}"]
+        for related_train in composition_train_depart(DATA_DICT, (jour, numero)):
+            date, id_name = related_train
+            contraintes[f"min_DEB_depart_{jour}_{numero}_arrivee_{date}_{id_name}_MINIMUM"] = model.addConstr(
+                variables[f"min_DEB_{jour}_{numero}"] <= variables[f"Train_ARR_{date}_{id_name}_DEB"],
+                name = f"min_DEB_depart_{jour}_{numero}_arrivee_{date}_{id_name}_MINIMUM"
+        )
 
-MODEL.update()
-# MODEL.display()
-# MODEL.params.OutputFlag = 0
-MODEL.optimize()
+    return epsilon_minimum
